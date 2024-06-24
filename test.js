7375da7a99abacbc582c49b85c19104dd072423f
ldgonzalez26 <ldgonzalezmedina@gmail.com>
2024-06-24 15:07:12 -0500

    solved tests
    Signed-off-by: ldgonzalez26 <ldgonzalezmedina@gmail.com>

--- test.js
+++
@@ -1,5 +0,0 @@
-const test = 'vegetables';
-const sourceParts = test.split('/');
-console.log(sourceParts);
-const sourceDir = sourceParts.slice(0, -1).join('/');
-console.log('Hi', sourceDir);
--- test/directoryTree.test.js
+++ test/directoryTree.test.js
@@ -48,14 +48,22 @@ describe('Directory Tree', () => {
   test('Invalid DELETE command', () => {
     runTest(
       ['CREATE fruits', 'DELETE fruits/apples', 'LIST'],
-      ['CREATE fruits', 'Cannot delete fruits/apples - fruits/apples does not exist', 'LIST', 'fruits']
+      ['CREATE fruits', 'DELETE fruits/apples', 'Cannot delete fruits/apples - fruits/apples does not exist', 'LIST', 'fruits']
     );
   });
 
   test('Invalid MOVE command', () => {
     runTest(
       ['CREATE fruits', 'CREATE fruits/apples', 'MOVE fruits/apples non_existent', 'LIST'],
-      ['CREATE fruits', 'CREATE fruits/apples', 'Cannot move fruits/apples to non_existent - invalid path', 'LIST', 'fruits', 'apples']
+      [
+        'CREATE fruits',
+        'CREATE fruits/apples',
+        'MOVE fruits/apples non_existent',
+        'Cannot move fruits/apples to non_existent - invalid path',
+        'LIST',
+        'fruits',
+        'apples',
+      ]
     );
   });
 });
